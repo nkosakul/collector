1c0af1314ab0433afa84f3ff29073e5bf5c78961
nkosakul <nkosakul@3pc.de>
2021-03-08 15:01:31 +0100

    feat: creating global styles (normalize)

---
+++ src/styles/breakpoints.tsx
@@ -0,0 +1,13 @@
+const sizes: { mobile: number; tablet: number } = { mobile: 768, tablet: 1024 };
+
+export const devices: {
+  mobile: string;
+  mobileMin: string;
+  tablet: string;
+  tabletMin: string;
+} = {
+  mobile: `(max-width: ${sizes.mobile}px)`,
+  mobileMin: `(min-width: ${sizes.mobile + 1}px)`,
+  tablet: `(max-width: ${sizes.tablet}px)`,
+  tabletMin: `(min-width: ${sizes.tablet + 1}px)`,
+};
